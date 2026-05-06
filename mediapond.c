561aae739f89da76f484a22ff37ec176cba5f3a9
gui01E <guilhermevosne.2008@alunos.utfpr.edu.br>
2026-05-06 10:48:00 -0300

    Update mediapond.c

--- mediapond.c
+++ mediapond.c
@@ -4,25 +4,25 @@
 #include <ctype.h>
 #include <locale.h>
 
-int main(){
+int main() {
 
-    setlocale(LC_ALL, "Portuguese");
-    int i;
-    int n;
-    float a, b, c, resultado;                                                           
-    printf("Digite o Número de casos:");
-    scanf("%d", &n);
-    
-    for( i = 1; i <= n; i = i + 1 ) {
-        
-    printf("\nCaso %d:\n", i);
-    printf("Digite três números:");
-    scanf("%f %f %f", &a, &b, &c);
-    resultado = ((a*2 + b*3 + c*5) / 10);
-    printf("O valor da média ponderada é: %f\n", resultado);
-        
-        
-    }
-   
-    return 0;
+	setlocale(LC_ALL, "Portuguese");
+	int i;
+	int n;
+	float a, b, c, resultado;
+	printf("Digite o Número de casos:");
+	scanf("%d", &n);
+
+	for( i = 1; i <= n; i = i + 1 ) {
+
+		printf("\nCaso %d:\n", i);
+		printf("Digite três números:");
+		scanf("%f %f %f", &a, &b, &c);
+		resultado = ((a*2 + b*3 + c*5) / 10);
+		printf("O valor da média ponderada é: %f\n", resultado);
+
+
+	}
+
+	return 0;
 }
